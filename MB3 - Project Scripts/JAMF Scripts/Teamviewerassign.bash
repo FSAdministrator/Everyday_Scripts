30c57a54729617874d1f3312e72e022a68f05ec7
Felix Saville <36466222+FelixSaville@users.noreply.github.com>
2024-07-11 10:13:10 +1200

    Create Teamviewerassign.bash
    Initial script creation

---
+++ MB3 - Project Scripts/JAMF Scripts/Teamviewerassign.bash
@@ -0,0 +1,19 @@
+################################################################################
+#  ABOUT THIS SCRIPT
+#
+#  NAME
+#    TeamViewerAssignment.sh
+#
+#  SYNOPSIS
+#    ./TeamViewerAssignment.sh
+#
+#  DESCRIPTION
+#    Uses the built in API calls to catalogue the device into mb3's enterprise management platform. 
+#    This device will be placed into .support/New Deployment
+#
+#  DEPLOYED
+#   OJC, IPU
+#
+################################################################################
+
+/Applications/TeamViewerHost.app/Contents/Helpers/TeamViewer_Assignment -api-token 7349396-QHbMhuxs7z9KfmHpO5m9 -grant-easy-access
